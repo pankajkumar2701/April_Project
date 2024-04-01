5158e55e2c1581f78707a3bb38078989a5f490a0
Pankaj Tiwari <ptiwari@appsnovel.com>
2024-04-01 10:34:10 +0530

    Upload files from codezen

---
+++ API/Filter/FilterCriteria.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace April_Project.Filter
+{
+    public class FilterCriteria
+    {
+        public string PropertyName { get; set; }
+        public string Operator { get; set; }
+        public string Value { get; set; }
+    }
+}
